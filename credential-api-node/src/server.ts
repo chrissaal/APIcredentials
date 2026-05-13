f15815f3f4e6b09acffe1768ab4240f641cbadfe
Christian Sánchez Alcántar <christian.sanchez.alcantar@kairosds.com>
2026-05-13 09:53:57 -0600

    Remove combined Node credential API example

--- credential-api-node/src/server.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "./config/env.js";
-import { createApp } from "./app.js";
-import { createTokenValidator } from "./idp/index.js";
-
-const app = createApp(createTokenValidator());
-
-app.listen(env.PORT, () => {
-  console.log(`Credential API listening on port ${env.PORT}`);
-});
